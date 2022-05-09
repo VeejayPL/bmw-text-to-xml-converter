680d7b0fcb6834e4ed8c11d25981cba9fa68ff30
VeejeyPL <wojciech.jurkowski4@gmail.com>
2022-05-09 14:32:35 +0200

    Add button funcionality

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 // Text area will adjust its height to the pasted text rows
+// This function is assigned to oninput in HTML doc
 function auto_height(elem) {
   elem.style.height = "1px";
   elem.style.height = elem.scrollHeight + "px";
 }
 
 // Get pasted text and store it as an array
+// This function is assigned to oninput in HTML doc
 let text;
 function getValue(elem) {
   return (text = document.querySelector("#textarea").value.split("\n"));
@@ -24,6 +26,16 @@ function filterText(text) {
 // Add XML tags to codes
 let xml;
 function addXmlTag(codes) {
-  xml = codes.map((element) => `<ns1:saCode>${element}</ns1:saCode>`);
+  xml = codes
+    .map((element) => `<ns1:saCode>${element}</ns1:saCode>\n`)
+    .join("");
   return xml;
 }
+// Assign functions to button and append converted text to textarea
+const button = document.querySelector("#convert");
+button.addEventListener("click", () => {
+  filterText(text);
+  document.querySelector("#textarea").value = `<ns1:saCodes>\n${addXmlTag(
+    codes
+  )}</ns1:saCodes>`;
+});
