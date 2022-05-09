9bad21083aefe61ab60e2aad75496e18f2597a5e
VeejeyPL <wojciech.jurkowski4@gmail.com>
2022-05-09 11:33:28 +0200

    Add getValue() function to store pasted text in variable

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
+// Text area will adjust its height to the pasted text rows
 function auto_height(elem) {
   elem.style.height = "1px";
   elem.style.height = elem.scrollHeight + "px";
 }
+
+// Get pasted text and store it in variable as string
+let text;
+function getValue(elem) {
+  return (text = document.querySelector("#textarea").value);
+}
+
+// Convert text to array
+
+// Filter array for strings starting from 0 and with length of four characters
