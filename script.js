6c008ba35a551f399887381e79e3e9eee6ca65de
VeejeyPL <wojciech.jurkowski4@gmail.com>
2023-03-21 13:42:09 +0100

    Implemented color theme switch

--- script.js
+++ script.js
@@ -31,6 +31,7 @@ function addXmlTag(codes) {
     .join("");
   return xml;
 }
+
 // Assign functions to button and append converted text to textarea
 const button = document.querySelector("#button");
 button.addEventListener("click", () => {
@@ -39,3 +40,29 @@ button.addEventListener("click", () => {
     codes
   )}</ns1:saCodes>`;
 });
+
+// Theme switcher
+const colorThemes = document.querySelectorAll('[name="theme"]');
+
+function storeTheme(theme) {
+  localStorage.setItem("theme", theme);
+}
+
+function setTheme() {
+  const activeTheme = localStorage.getItem("theme");
+  colorThemes.forEach((themeOption) => {
+    if (themeOption.id === activeTheme) {
+      themeOption.checked = true;
+    }
+  });
+  document.documentElement.className = activeTheme;
+}
+
+colorThemes.forEach((themeOption) => {
+  themeOption.addEventListener("click", () => {
+    storeTheme(themeOption.id);
+    document.documentElement.className = themeOption.id;
+  });
+});
+
+document.onload = setTheme();
